336aa439943dc2f6aa96c3ed85d0c8e98a81ca1c
Min Thu Kyaw <minthukyaw@uit.edu.mm>
2020-10-03 03:54:51 +0630

    feat: search box added

--- components/header/Header.vue
+++ components/header/Header.vue
@@ -1,49 +1,54 @@
 <template>
-  <b-container>
-    <b-navbar toggleable="lg" variant="faded" type="light">
-      <b-navbar-brand href="#">
-        <img src="/logo.png" height="100" alt="">
-      </b-navbar-brand>
-      <button
-        class="navbar-toggler collapsed"
-        target="nav-collapse"
-      >
-        <span class="line" />
-        <span class="line" />
-        <span class="line" />
-      </button>
-      <b-navbar-toggle target="nav-collapse">
-        <span class="navbar-toggler-icon">
-          <i class="fas fa-bars" style="color:#fff; font-size:28px;" />
-        </span>
-      </b-navbar-toggle>
+  <div>
+    <b-container>
+      <b-navbar toggleable="lg" variant="faded" type="light">
+        <b-navbar-brand href="#">
+          <img src="/logo.png" height="100" alt="">
+        </b-navbar-brand>
+        <button
+          class="navbar-toggler collapsed"
+          target="nav-collapse"
+        >
+          <span class="line" />
+          <span class="line" />
+          <span class="line" />
+        </button>
+        <b-navbar-toggle target="nav-collapse">
+          <span class="navbar-toggler-icon">
+            <i class="fas fa-bars" style="color:#fff; font-size:28px;" />
+          </span>
+        </b-navbar-toggle>
 
-      <b-collapse id="nav-collapse" is-nav>
-        <!-- <NavItem class="ml-auto" /> -->
-        <!-- Right aligned nav items -->
-        <b-navbar-nav class="ml-auto">
-          <ContactUs class="d-none d-md-block d-lg-block" />
-        </b-navbar-nav>
-      </b-collapse>
-    </b-navbar>
-    <b-navbar toggleable="lg" variant="faded" type="light">
-      <b-collapse id="nav-collapse" is-nav>
-        <NavItem class="ml-auto" />
-        <b-navbar-nav class="ml-auto">
-          <ContactUs class="d-md-none" />
-        </b-navbar-nav>
-      </b-collapse>
-    </b-navbar>
-  </b-container>
+        <b-collapse id="nav-collapse" is-nav>
+          <Search class="ml-auto d-none d-md-block d-lg-block" />
+          <!-- Right aligned nav items -->
+          <b-navbar-nav class="ml-auto">
+            <ContactUs class="d-none d-md-block d-lg-block" />
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
+      <b-navbar toggleable="lg" variant="faded" type="light">
+        <b-collapse id="nav-collapse" is-nav>
+          <NavItem class="ml-auto" />
+          <b-navbar-nav class="ml-auto">
+            <ContactUs class="d-md-none" />
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
+    </b-container>
+    <Search class="ml-auto d-md-none" />
+  </div>
 </template>
 <script>
 
 import NavItem from '@/components/header/NavItem'
+import Search from '@/components/header/SearchBox'
 import ContactUs from '@/components/header/ContactUs'
 export default {
   components: {
     NavItem,
-    ContactUs
+    ContactUs,
+    Search
   }
 }
 </script>
