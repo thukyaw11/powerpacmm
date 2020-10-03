74c8706cb7ab5ffc446e500fc510e7d411ceebab
Nine-Ball9 <ballads9ball@gmail.com>
2020-10-03 09:53:49 +0630

    head

--- components/header/Header.vue
+++ components/header/Header.vue
@@ -1,7 +1,7 @@
 <template>
-  <b-container >
+  <b-container style="padding-top:0px; margin-top:0px">
     <b-navbar toggleable="lg" variant="faded" type="light" style="padding:0px">
-      <b-navbar-brand href="#" style="padding:0px" >
+      <b-navbar-brand href="#" >
         <img src="/logo.png" height="100" alt="">
       </b-navbar-brand>
       <button
@@ -18,50 +18,47 @@
         </span>
       </b-navbar-toggle>
 
-        <b-collapse id="nav-collapse" is-nav>
-          <Search class="ml-auto d-none d-md-block d-lg-block" />
-          <!-- Right aligned nav items -->
-          <b-navbar-nav class="ml-auto">
-            <ContactUs class="d-none d-md-block d-lg-block" />
-          </b-navbar-nav>
-        </b-collapse>
-      </b-navbar>
-      <b-navbar toggleable="lg" variant="faded" type="light">
-        <b-collapse id="nav-collapse" is-nav>
-          <NavItem class="ml-auto" />
-          <b-navbar-nav class="ml-auto">
-            <ContactUs class="d-md-none" />
-          </b-navbar-nav>
-        </b-collapse>
-      </b-navbar>
-    </b-container>
-    
-  </div>
+      <b-collapse id="nav-collapse" is-nav>
+        <!-- <NavItem class="ml-auto" /> -->
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          <ContactUs class="d-none d-md-block d-lg-block" />
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+    <b-navbar toggleable="lg" variant="faded" type="light">
+      <b-collapse id="nav-collapse" is-nav>
+        <NavItem class="ml-auto" />
+        <b-navbar-nav class="ml-auto">
+          <ContactUs class="d-md-none" />
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </b-container>
 </template>
 <script>
-import NavItem from "@/components/header/NavItem";
-import Search from "@/components/header/SearchBox";
-import ContactUs from "@/components/header/ContactUs";
+
+import NavItem from '@/components/header/NavItem'
+import ContactUs from '@/components/header/ContactUs'
 export default {
   components: {
     NavItem,
-    ContactUs,
-    Search,
-  },
-};
+    ContactUs
+  }
+}
 </script>
 <style scoped>
-.navbar-toggler {
-  width: 47px;
-  height: 34px;
-  outline: none;
-  border: none;
+.navbar-toggler{
+    width: 47px;
+    height: 34px;
+    outline: none;
+    border:none;
 }
-.navbar-toggler .line {
-  width: 100%;
-  float: left;
-  height: 3px;
-  background-color: #fff;
-  margin-bottom: 5px;
+.navbar-toggler .line{
+    width: 100%;
+    float: left;
+    height: 3px;
+    background-color: #fff;
+    margin-bottom: 5px;
 }
 </style>
