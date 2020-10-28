a58589f0defdea8b4f2744af8110f9ed517c0999
thukyaw11 <helloeleven968@gmail.com>
2020-10-28 22:42:00 +0630

    feat: page id changed

--- plugins/vue-fb-customer-chat.js
+++ plugins/vue-fb-customer-chat.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import VueFbCustomerChat from 'vue-fb-customer-chat'
 
 Vue.use(VueFbCustomerChat, {
-  page_id: 108697137620643, //  change 'null' to your Facebook Page ID,
+  page_id: 336342737050289, //  change 'null' to your Facebook Page ID,
   theme_color: '#4685CC', // theme color in HEX
   locale: 'en_US' // default 'en_US'
 })
